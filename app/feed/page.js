91eb4b9cd318597b355fdbf169210f9423795c86
KubaJarmolowicz <kuba.jarmolowicz92@gmail.com>
2025-06-15 12:26:13 +0200

    Add metadata generation

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,5 +1,14 @@
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from "@/components/posts";
+import { getPosts } from "@/lib/posts";
+
+export const generateMetadata = async () => {
+  const posts = await getPosts();
+
+  return {
+    title: `Browse all our ${posts.length} posts.`,
+    description: "Browse all our posts.",
+  };
+};
 
 export default async function FeedPage() {
   const posts = await getPosts();
